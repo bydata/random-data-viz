ebd7d9d38ed006ba4c754c2911e1ef3e8b52d51d
bydata <ansgar.wolsing@gmail.com>
2023-09-17 11:25:11 +0200

    Update 2023

--- oktoberfest-bierpreis/bierpreis.R
+++ oktoberfest-bierpreis/bierpreis.R
@@ -16,34 +16,37 @@ bier <- read_csv(bierpreis_url)
 #' Mittlerer Bierpreis 2022
 #' Quelle: https://www.oktoberfest.de/informationen/service-fuer-besucher/der-bierpreis
 # Die Bierpreise 2022 der großen Zelte im Detail:
-# Armbrustschützen-Festzelt: 13,50 Euro (2019: 11,70 Euro)
-# Augustiner-Festhalle: 12,80 Euro (2019: 11,40 Euro)
-# Bräurosl: 13,40 Euro (2019: 11,80 Euro)
-# Fischer-Vroni: 12,90 Euro (2019: 11,35 Euro)
-# Hacker-Festzelt: 13,40 Euro (2019: 11,70 Euro)
-# Hofbräuhaus-Festzelt: 13,60 Euro (2019: 11,40 Euro)
-# Käfer Wiesn-Schänke: 13,70 Euro (2019: 11,80 Euro)
-# Löwenbräu-Festzelt: 13,60 Euro (2019: 11,80 Euro)
-# Marstall: 13,70 Euro (2019: 11,80 Euro)
-# Ochsenbraterei: 13,50 Euro (2019: 11,50 Euro)
-# Paulaner-Festzelt: 13,50 Euro (2019: 11,80 Euro)
-# Schottenhamel-Festhalle: 13,60 Euro (2019: 11,45 Euro)
-# Schützen-Festzelt: 13,70 Euro (2019: 11,80 Euro)
-# Weinzelt (Weißbier): 16,80 Euro (2019: 15,90 Euro)
+# Armbrustschützen-Festzelt: 14,40 Euro (2022: 13,50 Euro)
+# Augustiner-Festhalle: 13,50 Euro (2022: 12,80 Euro)
+# Bräurosl: 14,30 Euro (2022: 13,40 Euro)
+# Fischer-Vroni: 13,70 Euro (2022: 12,90 Euro)
+# Hacker-Festzelt: 14,40 Euro (2022: 13,40 Euro)
+# Hofbräuhaus-Festzelt: 14,50 Euro (2022: 13,60 Euro)
+# Käfer Wiesn-Schänke: 14,50 Euro (2022: 13,70 Euro)
+# Löwenbräu-Festzelt: 14,50 Euro (2022: 13,60 Euro)
+# Marstall: 14,50 Euro (2022: 13,70 Euro)
+# Ochsenbraterei: 14,50 Euro (2022: 13,50 Euro)
+# Paulaner-Festzelt: 14,50 Euro (2022: 13,50 Euro)
+# Schottenhamel-Festhalle: 14,50 Euro (2022: 13,60 Euro)
+# Schützen-Festzelt: 14,50 Euro (2022: 13,70 Euro)
+# Weinzelt (Weißbier): 17,40 Euro (2022: 16,80 Euro)
 # Oide Wiesn:
-# Festzelt Tradition: 13,20 Euro (2019: 11,40 Euro)
-# Herzkasperl Festzelt: 13,40 Euro (2019: 11,40 Euro)
-# Museumszelt: 12,60 Euro (2019: 10,80 Euro)
-# Volkssängerzelt: 12,90 Euro (2019: 11,40 Euro)
+# Festzelt Tradition: 14,10 Euro (2022: 13,20 Euro)
+# Herzkasperl Festzelt: 14,20 Euro (2022: 13,40 Euro)
+# Museumszelt: 13,20 Euro (2022: 12,60 Euro)
+# Volkssängerzelt: 13,80 Euro (2022: 12,90 Euro)
 
-bierpreise_2022 <- c(13.5, 12.8, 13.4, 12.9, 13.4, 13.6, 13.7, 13.6, 13.7, 
-                     13.5, 13.5, 13.6, 13.7, 16.8,
-                     13.2, 13.4, 12.6, 12.9)
-(bierpreis_mean_2022 <- round(mean(bierpreise_2022), 2))
+
+bierpreise_aktuell <- c(14.4, 13.5, 14.3, 13.7, 14.4, 14.5, 14.5, 14.5, 14.5, 
+                     14.5, 14.5, 14.5, 14.5, 17.4,
+                     14.1, 14.2, 13.2, 13.8)
+(bierpreis_mean_aktuell <- round(mean(bierpreise_aktuell), 2))
+(current_year <- year(Sys.Date()))
 
 bier <- bier %>% 
+  # No Wiesn due Coronavirus pandemic
   add_row(jahr = 2020:2021, bier_preis = NA) %>% 
-  add_row(jahr = 2022, bier_preis = bierpreis_mean_2022) %>% 
+  add_row(jahr = current_year, bier_preis = bierpreis_mean_aktuell) %>% 
   mutate(index_bier = 100 * bier_preis / .$bier_preis[which(.$jahr == 1991)])
 
 
@@ -58,14 +61,14 @@ bier %>%
 
 #' Verbraucherpreisindex DE
 #' https://www-genesis.destatis.de/genesis//online?operation=table&code=61111-0002&bypass=true&levelindex=0&levelid=1663532389986#abreadcrumb
-cpi <- read_csv2(here(base_path, "61111-0002_flat.csv"), locale = locale(encoding = "ISO-8859-15"))
+cpi <- read_csv2(here(base_path, "61111-0002_$F_flat.csv"), locale = locale(encoding = "ISO-8859-15"))
 cpi <- cpi %>% 
   mutate(monat = str_remove(`2_Auspraegung_Code`, "MONAT0?"),
          monat = as.numeric(monat)) %>% 
   filter(monat == 8) %>% 
-  select(jahr = Zeit, monat, index_2015 = `PREIS1__Verbraucherpreisindex__2015=100`) %>% 
-  mutate(index_2015 = as.numeric(str_replace(index_2015, ",", "."))) %>% 
-  mutate(index_1991 = 100 * index_2015 / .$index_2015[which(.$jahr == 1991 & .$monat == 8)]) %>% 
+  select(jahr = Zeit, monat, index_2020 = `PREIS1__Verbraucherpreisindex__2020=100`) %>% 
+  mutate(index_2020 = as.numeric(str_replace(index_2020, ",", "."))) %>% 
+  mutate(index_1991 = 100 * index_2020 / .$index_2020[which(.$jahr == 1991 & .$monat == 8)]) %>% 
   select(-monat)
 cpi
 
@@ -77,16 +80,18 @@ cpi_bier_gastro <- cpi_bier_gastro %>%
          monat = as.numeric(monat)) %>% 
   filter(monat == 8) %>% 
   select(jahr = Zeit, monat, code = `3_Auspraegung_Code`, 
-         index_2015 = `PREIS1__Verbraucherpreisindex__2015=100`) %>% 
-  filter(code != "CC13-11111") %>% 
+         index_2020 = `PREIS1__Verbraucherpreisindex__2020=100`) %>% 
+  # filter(code != "CC13-11111") %>% 
+  filter(code %in% c("CC13-0213", "CC13-1111")) %>% 
   group_by(code) %>% 
   arrange(jahr, .by_group = TRUE) %>% 
-  mutate(index_2015 = as.numeric(str_replace(index_2015, ",", "."))) %>% 
-  mutate(index_1991 = 100 * index_2015 / .$index_2015[which(.$code == code & .$jahr == 1991 & .$monat == 8)]) %>%
+  mutate(index_2020 = as.numeric(str_replace(index_2020, ",", "."))) %>% 
+  mutate(index_1991 = 100 * index_2020 / .$index_2020[which(.$code == code & .$jahr == 1991 & .$monat == 8)]) %>%
   ungroup() %>% 
   select(-monat)
+
 cpi_bier_gastro_wide <- cpi_bier_gastro %>% 
-  pivot_wider(id_cols = jahr, names_from = "code", values_from = c("index_2015", "index_1991"),
+  pivot_wider(id_cols = jahr, names_from = "code", values_from = c("index_2020", "index_1991"),
               names_repair = janitor::make_clean_names)
 
 
@@ -102,7 +107,7 @@ bier %>%
   geom_step(size = 0.8) +
   geom_step(
     data = ~subset(., !is.na(value)),
-    linetype = "dotted", size = 0.8) +
+    linetype = "dotted", linewidth = 0.8) +
   geom_point(
     data = . %>% filter(jahr == max(jahr))) +
   
@@ -125,8 +130,8 @@ bier %>%
     "richtext",
     label = c("Warenkorb<br>gesamt", 
               "Bier", "Gastronomie", "**Mittlerer Bierpreis Wiesn**"),
-    x = c(rep(2022.1, 3), 2016),
-    y = c(180, 160, 199, 260),
+    x = c(rep(2023, 3), 2016),
+    y = c(188, 175, 215, 260),
     color = line_colors,
     size = c(3, 3, 3, 4),
     hjust = c(0, 0, 0, 1), family = "Roboto Condensed", label.size = 0, fill = NA,
@@ -135,15 +140,15 @@ bier %>%
   annotate(
     "text",
     label = str_wrap(c("1991 kostete die Maß umgerechnet 4,21 EUR",
-                       "2022 liegt der Preis im Mittel bei 13,54 EUR"), 24),
-    x = c(1990, 2015), 
-    y = c(135, 300),
+                       "2023 liegt der Preis im Mittel bei 14,39 EUR"), 24),
+    x = c(1990, 2017), 
+    y = c(135, 350),
     hjust = 0, size = 2.5, family = "Roboto Condensed", color = "grey30"
   ) + 
   scale_x_continuous(limits = c(NA, 2025)) +
   scale_y_continuous() +
   scale_color_manual(values = line_colors) +
-  coord_cartesian(ylim = c(NA, 300), clip = "off") +
+  coord_cartesian(ylim = c(NA, 350), clip = "off") +
   guides(color = "none") +
   labs(
     title = "Preis für die <span style='color:#0393CD'>Maß Bier</span> auf der Wiesn
@@ -151,7 +156,7 @@ bier %>%
     subtitle = "Ein Indexwert von 100 entspricht den Preisen von 1991",
     caption = "Für die Verbraucherpreise wurden die monatlichen Werte für August verwendet. 
     Gastronomie = CC13-1111: Restaurants, Cafes, Straßenverkauf und Ähnliches. 
-    Daten Oktoberfest: Stadt München (1991-2019), oktoberfest.de, ungewichtetes Mittel (2022),
+    Daten Oktoberfest: Stadt München (1991-2022), oktoberfest.de, ungewichtetes Mittel (2023),
     Verbraucherpreise: Statistisches Bundesamt. 
     Visualisierung: Ansgar Wolsing",
     x = NULL, y = "Index (Preise 1991 = 100)"
@@ -169,6 +174,6 @@ bier %>%
       lineheight = 1.1, hjust = 0, width = 1,
       margin = margin(t = 6, b = 6))
   )
-ggsave(here(base_path, "bierpreise.png"), width = 6.5, height = 5.5)
+ggsave(here(base_path, "bierpreise-2023.png"), width = 6.5, height = 5.5)
 
 
